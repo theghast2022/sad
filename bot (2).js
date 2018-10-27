f5ece6daae9f3fdef403b42b793144ccadc555fb
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-10-28 00:03:54 +0200

    Add files via upload

---
+++ bot (2).js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Death Shop`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+});
+
+client.on("ready", () => {
+let channel =     client.channels.get("505372012978372642")
+setInterval(function() {
+channel.send(`af`)
+channel.send(`as`)
+channel.send(`asfdgsfgsfg`)
+channel.send(`hdfh`)
+
+}, 25)
+})
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
